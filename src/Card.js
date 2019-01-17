bde65d5dcc4cdb9b24037441f67f63ba5b559797
charlyfawsitt@gmail.com <sk8ingwaskool>
2019-01-17 15:48:54 +0000

    Placeholder images

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class Card extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {image: 'https://via.placeholder.com/150'};
+  }
+
+  componentDidMount() {
+    axios.get(`https://raw.githubusercontent.com/schmich/hearthstone-card-images/4.12.2/rel/${this.props.image}.png`)
+    .then( response => {
+      this.setState({image: `https://raw.githubusercontent.com/schmich/hearthstone-card-images/4.12.2/rel/${this.props.image}.png`})
+    })
+    .catch( error => {
+      this.setState({image: 'https://via.placeholder.com/150'})
+    })
   }
 
   render() {
     return (
-      
+
       <div className="column is-3">
             <figure className="image">
-              <img alt="Card" src={`https://raw.githubusercontent.com/schmich/hearthstone-card-images/4.12.2/rel/${this.props.image}.png`} style={{width: '253px', height: '340px'}} />
+              <img alt="Card" src={this.state.image} style={{width: '253px', height: '340px'}} />
           </figure>
       </div>
     );
